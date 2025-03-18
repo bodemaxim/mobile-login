05e175fbdafda79d9d1212929da2b733a78366fe
bodemaxim <bodemaxim@gmail.com>
2025-03-18 14:55:14 +0300

    i18n error

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import VueI18n from '@intlify/unplugin-vue-i18n/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/mobile-login',
-  plugins: [vue(), vueJsx(), VueDevTools()],
+  plugins: [
+    vue(),
+    vueJsx(),
+    VueDevTools(),
+    VueI18n({
+      runtimeOnly: false
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
